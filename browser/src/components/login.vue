6106afc656183cf7a9d70f889f7a16fb0ffe9aff
yxie@nbai.io <yxie@nbai.io>
2023-09-14 15:13:06 +0800

    Add text prompt

--- browser/src/components/login.vue
+++ browser/src/components/login.vue
@@ -12,6 +12,9 @@
                 </el-form-item>
                 <el-form-item>
                     <el-button icon="iconfont icon-denglutongji" @click="submitForm('ruleForm')"></el-button>
+                    <p>To request a trial account, please
+                        <a href="mailto:contact@fogmeta.com"> contact us via email</a>.
+                        <br /> if you don't have an account yet.</p>
                 </el-form-item>
             </el-form>
         </div>
@@ -25,7 +28,7 @@
 import axios from 'axios'
 import Moment from 'moment'
 export default {
-    data() {
+    data () {
         var validateAccesskey = (rule, value, callback) => {
             if (value === '') {
                 callback(new Error('Please fill in this field.'));
@@ -61,19 +64,19 @@ export default {
         };
     },
     computed: {
-        email() {
+        email () {
             return this.$store.state.user.email
         },
     },
     watch: {
         $route: function (to, from) {
-            if(this.bodyWidth){
+            if (this.bodyWidth) {
 
             }
         }
     },
     methods: {
-        submitForm(formName) {
+        submitForm (formName) {
             this.$refs[formName].validate((valid) => {
                 if (valid) {
                     let _this = this;
@@ -87,19 +90,19 @@ export default {
                         id: 1,
                         jsonrpc: "2.0",
                         method: "web.Login",
-                        params:{
+                        params: {
                             username: _this.ruleForm.Accesskey,
                             password: _this.ruleForm.Secretkey
                         }
                     }
 
-                    axios.post(postUrl, dataLogin,{headers: {}}).then((response) => {
+                    axios.post(postUrl, dataLogin, { headers: {} }).then((response) => {
                         let json = response.data
                         let error = json.error
                         let result = json.result
                         if (error) {
                             _this.$message.error(error.message);
-                            _this.loginLoad=false
+                            _this.loginLoad = false
                             return false
                         }
 
@@ -111,7 +114,7 @@ export default {
                             location.reload()
                         }
 
-                        _this.loginLoad=false
+                        _this.loginLoad = false
                         sessionStorage.fs3MinioLoginAccessToken = result.token
                         _this.$store.state.user.accessToken = result.token
                         console.log('login token: ', _this.$store.state.user.accessToken)
@@ -123,7 +126,7 @@ export default {
 
                     }).catch(function (error) {
                         console.log(error);
-                        _this.loginLoad=false
+                        _this.loginLoad = false
                     });
                 } else {
                     console.log('error submit!!');
@@ -131,110 +134,118 @@ export default {
                 }
             });
         },
-        resetForm(formName) {
+        resetForm (formName) {
             this.$refs[formName].resetFields();
         }
     },
-    mounted() {
+    mounted () {
         this.location = window.location.host;
     }
 };
 </script>
 <style  lang="scss" scoped>
-.login{
-    height: 100%;
-    min-height: 500px;
-    background: #002a37;
-    text-align: center;
-    .formContent{
-        width: 80%;
-        max-width: 500px;
-        height: calc(100% - 100px);
-        margin: auto;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-wrap: wrap;
-        .el-form /deep/{
-            width: 100%;
-            margin: auto;
-            .el-form-item{
-                width: 100%;
-                .title{
-                    color: #8e8e8e;
-                    line-height: 1;
-                }
-                .el-input{
-                    .el-input__inner{
-                        background-color: transparent;
-                        box-shadow: none;
-                        border: 0;
-                        border-bottom: 1px solid rgba(255, 255, 255, 0.1);
-                        color: #fff;
-                        text-align: center;
-                    }
-                }
-                .el-button{
-                    width: 0.55rem;
-                    height: 0.55rem;
-                    padding: 0;
-                    margin: 0.15rem auto 0;
-                    background-color: transparent;
-                    border: 1px solid #fff;
-                    border-radius: 50%;
-                    opacity: 0.3;
-                    font-size: 0.2rem;
-                    font-family: inherit;
-                    color: #fff;
-                    i{
-                        font-size: 0.22rem;
-                        font-weight: bold;
-                    }
-                    &:hover{
-                        opacity: 0.8;
-                    }
-                    @media screen and (max-width:600px){
-                        font-size: 22px;
-                        width: 40px;
-                        height: 40px;
-                        i{
-                            font-size: 22px;
-                        }
-                    }
-                }
-            }
+.login {
+  height: 100%;
+  min-height: 500px;
+  background: #002a37;
+  text-align: center;
+  .formContent {
+    width: 80%;
+    max-width: 500px;
+    height: calc(100% - 100px);
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    .el-form /deep/ {
+      width: 100%;
+      margin: auto;
+      .el-form-item {
+        width: 100%;
+        .title {
+          color: #8e8e8e;
+          line-height: 1;
         }
+        .el-input {
+          .el-input__inner {
+            background-color: transparent;
+            box-shadow: none;
+            border: 0;
+            border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+            color: #fff;
+            text-align: center;
+          }
+        }
+        .el-button {
+          width: 0.55rem;
+          height: 0.55rem;
+          padding: 0;
+          margin: 0.15rem auto 0;
+          background-color: transparent;
+          border: 1px solid #fff;
+          border-radius: 50%;
+          opacity: 0.3;
+          font-size: 0.2rem;
+          font-family: inherit;
+          color: #fff;
+          i {
+            font-size: 0.22rem;
+            font-weight: bold;
+          }
+          &:hover {
+            opacity: 0.8;
+          }
+          @media screen and (max-width: 600px) {
+            font-size: 22px;
+            width: 40px;
+            height: 40px;
+            i {
+              font-size: 22px;
+            }
+          }
+        }
+        p {
+          margin: 0.2rem 0 0;
+          color: rgba(255, 255, 255, 0.4);
+          line-height: 1.3;
+          word-break: break-word;
+          a {
+            color: inherit;
+            text-decoration: underline;
+          }
+        }
+      }
     }
-    .login_bot{
-        height: 100px;
-        padding: 0 0.5rem;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        .left{
-            font-size: 0.2rem;
-            color: rgba(255, 255, 255, 0.4);
-            line-height: 0.8rem;
-            @media screen and (max-width:600px){
-                font-size: 14px;
-            }
-        }
-        .right{
-            img{
-                height: 0.4rem;
-                margin: 0.2rem 0;
-                @media screen and (max-width:600px){
-                    height: 30px;
-                    margin: 15px 0 0;
-                }
-            }
-        }
+  }
+  .login_bot {
+    height: 100px;
+    padding: 0 0.5rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .left {
+      font-size: 0.2rem;
+      color: rgba(255, 255, 255, 0.4);
+      line-height: 0.8rem;
+      @media screen and (max-width: 600px) {
+        font-size: 14px;
+      }
     }
+    .right {
+      img {
+        height: 0.4rem;
+        margin: 0.2rem 0;
+        @media screen and (max-width: 600px) {
+          height: 30px;
+          margin: 15px 0 0;
+        }
+      }
+    }
+  }
 }
-@media screen and (max-width:1024px){
-
+@media screen and (max-width: 1024px) {
 }
-@media screen and (max-width: 600px){
-
+@media screen and (max-width: 600px) {
 }
 </style>
